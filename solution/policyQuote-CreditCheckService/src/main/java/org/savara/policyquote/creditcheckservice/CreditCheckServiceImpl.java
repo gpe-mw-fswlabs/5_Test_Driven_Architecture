2caec4aa111900961ef20571374e6ccd45d2279c
njoshi <nandanjoshi@Nandans-MacBook-Pro.local>
2014-10-31 14:29:31 -0700

    Initial commit for Lab 5

---
+++ solution/policyQuote-CreditCheckService/src/main/java/org/savara/policyquote/creditcheckservice/CreditCheckServiceImpl.java
@@ -0,0 +1,33 @@
+
+/**
+ * Please modify this class to meet your needs
+ * This class is not complete
+ */
+
+package org.savara.policyquote.creditcheckservice;
+
+import com.creditagency.creditcheck.CreditCheckRequest;
+import com.creditagency.creditcheck.CreditCheckResponse;
+
+import java.util.logging.Logger;
+
+@org.switchyard.component.bean.Service(CreditCheckService.class)
+public class CreditCheckServiceImpl implements CreditCheckService {
+
+    private static final Logger LOG = Logger.getLogger(CreditCheckServiceImpl.class.getName());
+
+    /* (non-Javadoc)
+     * @see org.savara.policyquote.creditcheckservice.CreditCheckService#creditCheck(com.creditagency.creditcheck.CreditCheckRequest  content )*
+     */
+    public com.creditagency.creditcheck.CreditCheckResponse creditCheck(com.creditagency.creditcheck.CreditCheckRequest content) {
+
+        LOG.info("creditCheck() content = "+content);
+        CreditCheckResponse ret=new CreditCheckResponse();
+
+        ret.setSsn(content.getSsn());
+
+        ret.setScore(500);
+        return (ret);
+    }
+
+}
---
+++ solution/policyQuote-DrivingRecordService/src/main/java/org/savara/policyquote/drivingrecordservice/DrivingRecordServiceImpl.java
@@ -0,0 +1,44 @@
+
+/**
+ * Please modify this class to meet your needs
+ * This class is not complete
+ */
+
+package org.savara.policyquote.drivingrecordservice;
+
+import java.util.logging.Logger;
+
+import com.dmv.drivingrecord.DrivingRecordResponse;
+
+@org.switchyard.component.bean.Service(DrivingRecordService.class)
+public class DrivingRecordServiceImpl implements DrivingRecordService {
+
+    private static final Logger LOG = Logger.getLogger(DrivingRecordServiceImpl.class.getName());
+
+    /* (non-Javadoc)
+     * @see org.savara.policyquote.drivingrecordservice.DrivingRecordService#drivingRecord(com.dmv.drivingrecord.DrivingRecordRequest  content )*
+     */
+    public com.dmv.drivingrecord.DrivingRecordResponse drivingRecord(com.dmv.drivingrecord.DrivingRecordRequest content) {
+
+        LOG.info("drivingRecord() content = "+content+" : if ssn = 555555555 introduce delay");        
+        
+        DrivingRecordResponse ret=new DrivingRecordResponse();
+        ret.setAge(content.getAge());
+        ret.setDlNumber(content.getDlNumber());
+        ret.setName(content.getName());
+        if ("666666666".equals(content.getSsn())) {
+            ret.setNumberOfAccidents(6);
+        } else  {
+            ret.setNumberOfAccidents(0);
+        }
+        ret.setNumberOfTickets(1);
+        ret.setSsn(content.getSsn());
+        if ("555555555".equals(ret.getSsn())) {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException ignore) {}
+        }
+        return (ret);
+    }
+
+}
